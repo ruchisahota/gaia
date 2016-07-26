b2eee56612fe9a9055a97f52277d3b8218545014
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-07-26 10:52:52 -0700

    generate the code

--- identities_registry.go
+++ identities_registry.go
@@ -7,15 +7,16 @@ import "github.com/aporeto-inc/elemental"
 func init() {
 
 	elemental.RegisterIdentity(MyNamespaceIdentity)
-	elemental.RegisterIdentity(MapNodeIdentity)
+	elemental.RegisterIdentity(HealthReportIdentity)
 	elemental.RegisterIdentity(DependencyMapIdentity)
 	elemental.RegisterIdentity(CertificateIdentity)
 	elemental.RegisterIdentity(FilePathIdentity)
+	elemental.RegisterIdentity(ServerIdentity)
 	elemental.RegisterIdentity(SystemCallIdentity)
 	elemental.RegisterIdentity(NamespaceIdentity)
 	elemental.RegisterIdentity(MapEdgeIdentity)
 	elemental.RegisterIdentity(PolicyRuleIdentity)
-	elemental.RegisterIdentity(ServerIdentity)
+	elemental.RegisterIdentity(MapNodeIdentity)
 	elemental.RegisterIdentity(AuthenticatorIdentity)
 	elemental.RegisterIdentity(ExternalServiceIdentity)
 	elemental.RegisterIdentity(TagIdentity)
