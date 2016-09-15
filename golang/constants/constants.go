95c019767cfe203cddeb21ffb041d0df3d739ec8
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-09-15 13:45:21 -0700

    new: added more constants for tags key to support volumes binding tags

--- golang/constants/constants.go
+++ golang/constants/constants.go
@@ -153,4 +153,13 @@ const (
 
 	// StatsTagKeyAporetoContextID represents the aporeto context ID.
 	StatsTagKeyAporetoContextID = "AporetoContextID"
+
+	// StatsTagKeyMountedPath represents the mounted path.
+	StatsTagKeyMountedPath = "mountedPath"
+
+	// StatsTagKeyMountPoint represents the mount point of the StatsTagKeyMountedPath.
+	StatsTagKeyMountPoint = "mountPoint"
+
+	// StatsTagKeyMountMode represents the mode of mount point.
+	StatsTagKeyMountMode = "mountMode"
 )
