eefc5be95b7a2e4e39d556fa88753e9222daeb62
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-09-11 10:35:56 -0700

    fixed: relations

--- barretmodels/v1/golang/relationships_registry.go
+++ barretmodels/v1/golang/relationships_registry.go
@@ -20,23 +20,7 @@ func init() {
 			"root": true,
 		},
 	}
-	relationshipsRegistry[EnforcerCertIdentity] = &elemental.Relationship{
-		AllowsUpdate: map[string]bool{
-			"root": true,
-		},
-		AllowsDelete: map[string]bool{
-			"root": true,
-		},
-		AllowsRetrieve: map[string]bool{
-			"root": true,
-		},
-		AllowsRetrieveMany: map[string]bool{
-			"root": true,
-		},
-		AllowsInfo: map[string]bool{
-			"root": true,
-		},
-	}
+	relationshipsRegistry[EnforcerCertIdentity] = &elemental.Relationship{}
 	relationshipsRegistry[RootIdentity] = &elemental.Relationship{}
 	relationshipsRegistry[TriremeCertIdentity] = &elemental.Relationship{
 		AllowsCreate: map[string]bool{
