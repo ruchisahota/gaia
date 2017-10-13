ce04b62f0ab75168c3e0cbb95d00c5a0f9d7475f
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-10-12 21:13:24 -0700

    someone remove zack model v1..

---
+++ zackmodels/v1/golang/relationships_registry.go
@@ -0,0 +1,24 @@
+package zackmodels
+
+import "github.com/aporeto-inc/elemental"
+
+const nodocString = "[nodoc]" // nolint: varcheck
+
+var relationshipsRegistry elemental.RelationshipsRegistry
+
+// Relationships returns the model relationships.
+func Relationships() elemental.RelationshipsRegistry {
+
+	return relationshipsRegistry
+}
+
+func init() {
+	relationshipsRegistry = elemental.RelationshipsRegistry{}
+
+	relationshipsRegistry[ReportIdentity] = &elemental.Relationship{
+		AllowsCreate: map[string]bool{
+			"root": true,
+		},
+	}
+	relationshipsRegistry[RootIdentity] = &elemental.Relationship{}
+}
