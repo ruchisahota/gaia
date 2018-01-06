dbef300bd142f25986e726ad8012441598bd338e
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-01-06 02:47:08 -0800

    fixed: limit the max len of proxyListenAddress

--- squallmodels/v1/golang/enforcerprofile.go
+++ squallmodels/v1/golang/enforcerprofile.go
@@ -385,6 +385,10 @@ func (o *EnforcerProfile) Validate() error {
 		errors = append(errors, err)
 	}
 
+	if err := elemental.ValidateMaximumLength("proxyListenAddress", o.ProxyListenAddress, 512, false); err != nil {
+		errors = append(errors, err)
+	}
+
 	if err := elemental.ValidateMaximumInt("receiverNumberOfQueues", o.ReceiverNumberOfQueues, 16, false); err != nil {
 		errors = append(errors, err)
 	}
@@ -728,6 +732,7 @@ var EnforcerProfileAttributesMap = map[string]elemental.AttributeSpecification{
 		Exposed:        true,
 		Filterable:     true,
 		Format:         "free",
+		MaxLength:      512,
 		Name:           "proxyListenAddress",
 		Orderable:      true,
 		Stored:         true,
@@ -1123,6 +1128,7 @@ var EnforcerProfileLowerCaseAttributesMap = map[string]elemental.AttributeSpecif
 		Exposed:        true,
 		Filterable:     true,
 		Format:         "free",
+		MaxLength:      512,
 		Name:           "proxyListenAddress",
 		Orderable:      true,
 		Stored:         true,
