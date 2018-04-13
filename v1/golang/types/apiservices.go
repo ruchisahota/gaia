a303c45132fff254c07027a922e9ebd8768ac7b0
Dimitri Stiliadis <dstiliadis@users.noreply.github.com>
2018-04-13 13:38:10 -0700

    Selectors are not mandatory (#128)

--- v1/golang/types/apiservices.go
+++ v1/golang/types/apiservices.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // IPList is a list of IP addresses
 type IPList []IPAddress
 
@@ -17,5 +22,32 @@ type ExposedAPI struct {
 	Scopes []string `json:"scopes" bson:"scopes" mapstructure:"scopes,omitempty"`
 }
 
+var (
+	// Proper format a port
+	portRegEx      = regexp.MustCompile("^([1-9]|[1-9][0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|65535)(:([1-9]|[1-9][0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|65535))?$")
+	exactPortRegEx = regexp.MustCompile("^([1-9]|[1-9][0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|65535)$")
+)
+
 // PortList is a list of ports for the service.
 type PortList []string
+
+// Validate validates an input slice of ports.
+func (p PortList) Validate() error {
+	for _, port := range p {
+		if !portRegEx.Match([]byte(port)) {
+			return fmt.Errorf("Invalid port definition: %s", p)
+		}
+	}
+	return nil
+}
+
+// ValidateExactPort will validate that the ports are exact and not ranges.
+// Required for HTTP services.
+func (p PortList) ValidateExactPort() error {
+	for _, port := range p {
+		if !exactPortRegEx.Match([]byte(port)) {
+			return fmt.Errorf("Invalid port definition: %s", p)
+		}
+	}
+	return nil
+}
