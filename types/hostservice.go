66d284edefa4e34ddb17f52e9b91a51e440eaead
Christophe Serafin <christophe.serafin@gmail.com>
2018-09-26 14:46:14 -0700

    validate host service name according to the regex. Fixes #189 (#190)

--- types/hostservice.go
+++ types/hostservice.go
@@ -1,6 +1,11 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+var regHostServiceName = regexp.MustCompile(`^[a-zA-Z0-9_:.$%/-]{0,64}$`)
 
 // HostService is a service associated with an enforcer profile for a host.
 type HostService struct {
@@ -20,9 +25,11 @@ func (h HostServicesList) Validate() error {
 		if len(s.Name) == 0 {
 			return fmt.Errorf("Host service names must be specified")
 		}
-		if len(s.Name) > 12 {
-			return fmt.Errorf("Host service names must cannot be more than 12 characters")
+
+		if !regHostServiceName.MatchString(s.Name) {
+			return fmt.Errorf(`Host service name must be less than 64 characters and contains only alphanumeric and the following characters: _, :, ., %%, / and -`)
 		}
+
 		if s.Services != nil {
 			if err := s.Services.Validate(); err != nil {
 				return err
