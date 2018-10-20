f15150b2c389b940aed69fda1ffd500192efab78
Christophe Serafin <christophe.serafin@gmail.com>
2018-10-19 19:11:18 -0700

    Added new types of parameters (#210)

--- types/automationstemplates.go
+++ types/automationstemplates.go
@@ -11,13 +11,17 @@ const (
 	AutomationTemplateParameterTypeFloat  AutomationTemplateParameterType = "float"
 	AutomationTemplateParameterTypeObject AutomationTemplateParameterType = "object"
 	AutomationTemplateParameterTypeArray  AutomationTemplateParameterType = "array"
+	AutomationTemplateParameterTypeFilter AutomationTemplateParameterType = "filter"
+	AutomationTemplateParameterTypeEnum   AutomationTemplateParameterType = "enum"
 )
 
 // An AutomationTemplateParameter represents an automation template parameter.
 type AutomationTemplateParameter struct {
-	Name         string                          `json:"name"`
-	Description  string                          `json:"description"`
-	Type         AutomationTemplateParameterType `json:"type"`
-	DefaultValue interface{}                     `json:"defaultValue"`
-	Required     bool                            `json:"required"`
+	Name           string                          `json:"name"`
+	Description    string                          `json:"description"`
+	Type           AutomationTemplateParameterType `json:"type"`
+	DefaultValue   interface{}                     `json:"defaultValue"`
+	Required       bool                            `json:"required"`
+	AllowedChoices map[string]interface{}          `json:"allowedChoices"`
+	Position       int                             `json:"position"`
 }
