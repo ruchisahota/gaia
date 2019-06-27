68492ca8bbd1df2e444ad009fedbc1a589c8694b
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-06-27 12:55:11 -0700

    update go.mod

---
+++ go.mod
@@ -0,0 +1,12 @@
+module go.aporeto.io/gaia
+
+go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/mitchellh/copystructure v1.0.0
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
+	github.com/stretchr/testify v1.3.0 // indirect
+	go.aporeto.io/elemental v1.55.0
+	gopkg.in/yaml.v2 v2.2.2
+)
---
+++ types/claims.go
@@ -0,0 +1,21 @@
+package types
+
+import jwt "github.com/dgrijalva/jwt-go"
+
+// MidgardClaims is a struct to represeting the data some a Midgard issued claims.
+type MidgardClaims struct {
+	Realm  string            `msgpack:"realm" json:"realm"`
+	Quota  int               `msgpack:"quota,omitempty" json:"quota,omitempty"`
+	Data   map[string]string `msgpack:"data" json:"data"`
+	Opaque map[string]string `msgpack:"opaque,omitempty" json:"opaque,omitempty"`
+
+	jwt.StandardClaims
+}
+
+// NewMidgardClaims returns a new Claims.
+func NewMidgardClaims() *MidgardClaims {
+	return &MidgardClaims{
+		Data:           map[string]string{},
+		StandardClaims: jwt.StandardClaims{},
+	}
+}
